433bd29a96f8eaeb76d921b5a79aae52313200b3
Alhadji Oumate <alhadjioum0@gmail.com>
2024-05-24 21:23:56 +0100

    mru cache algorithm

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-"""least recently used caching algorithm"""
+"""most recently used caching algorithm"""
 from base_caching import BaseCaching
 from collections import deque
 
 
-class LRUCache(BaseCaching):
-    """implementing the lru cache class"""
+class MRUCache(BaseCaching):
+    """implementing the mru cache class"""
     def __init__(self):
         """class constructure"""
         super().__init__()
-        self.queue = deque()
+        self.queue = []
 
     def put(self, key: str, item: str) -> None:
-        """insert into the lru cache"""
+        """insert into the mru cache"""
         if key and item:
             self.cache_data[key] = item
             if key in self.queue:
                 self.queue.remove(key)
             self.queue.append(key)
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                removed = self.queue.popleft()
+                removed = self.queue.pop(-2)
                 del self.cache_data[removed]
                 print(f"DISCARD: {removed}")
 
