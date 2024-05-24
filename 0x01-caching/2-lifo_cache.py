869e1db2ce477cda766ab0e8a6db4af8dba00bd6
Alhadji Oumate <alhadjioum0@gmail.com>
2024-05-24 18:55:47 +0100

    lifo caching algorithms

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -15,13 +15,13 @@ class LIFOCache(BaseCaching):
         """insert into lifo cache"""
         if key and item:
             self.cache_data[key] = item
-            if key in self.queue:
-                self.queue.remove(key)
-            self.queue.append(key)
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 removed = self.queue.pop()
                 del self.cache_data[removed]
                 print(f"DISCARD: {removed}")
+            if key in self.queue:
+                self.queue.remove(key)
+            self.queue.append(key)
 
     def get(self, key):
         """retrieve item from the cache"""
